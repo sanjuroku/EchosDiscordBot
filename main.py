35640c9ef382cdc59b258b471308bea4d88ddd8e
sanjuroku <kuroniko07@gmail.com>
2025-06-30 17:57:02 +0900

    added floof subreddit

--- main.py
+++ main.py
@@ -808,7 +808,7 @@ def is_valid_url(url: str) -> bool:
     return isinstance(url, str) and url.startswith("http")
 
 CUTE_SUBREDDITS = [
-    "aww", "Eyebleach", "rarepuppers", "AnimalsBeingDerps", "AnimalsOnReddit", "Catmemes", "PartyParrot", "Ornithology", "Birding", "birdwatching", "parrots", "BirdsArentReal"
+    "aww", "Eyebleach", "rarepuppers", "AnimalsBeingDerps", "AnimalsOnReddit", "Catmemes", "PartyParrot", "Ornithology", "Birding", "birdwatching", "parrots", "BirdsArentReal", "Floof"
 ]
 
 # 用于下拉选项
